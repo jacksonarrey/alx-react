9d0ecb89109f2804d1b8ae55a30be47507afa4c6
jacksonarrey <agborarrey93@gmail.com>
2023-07-28 11:08:26 +0100

    0x00-Webpack\task_1

---
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -0,0 +1,10 @@
+t path = require('path');
+
+module.exports = {
+	  mode: 'production',
+	  entry: './js/dashboard_main.js',
+	  output: {
+		      path: path.resolve(__dirname, 'public'),
+		      filename: 'bundle.js',
+		    },
+};
