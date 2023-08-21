c324241e9249512186ed9248cfeaad47729cd068
jackson arrey <agborarrey93@gmail.com>
2023-08-21 08:23:19 +0000

    0x06-React_Immutable

---
+++ 0x06-React_Immutable/2-nested.js
@@ -0,0 +1,7 @@
+mport { fromJS } from 'immutable';
+
+export default function accessImmutableObject(object, array) {
+  const mappedObj = fromJS(object);
+
+  return mappedObj.getIn(array, undefined);
+}
